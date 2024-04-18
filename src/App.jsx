ab69739704e258add5d06305a91f4585ae632360
LouWue <louwue@icloud.com>
2024-04-18 07:50:36 +0200

    Innitial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+<div id="home" class="first_page">
+  <div class="wrapper">
+    <div class="nav">
+    </div>
+    <div class="header header_home"> Name Vorname:   Jessica Bertschi</div>
+    <div class="content content_home">
+      Geburtsdatum: 3.10.2007 <br></br>
+      Verein: Turnverein Regensdorf<br></br>
+      Sportart: Geräteturnen<br></br>
+
+    </div>
+    <div class="image"></div>
+  </div>
+</div>
+
+    </>
+  )
+}
+
+export default App
